3d6feecba014562894f3fd7d59dae04088974604
Ted Vu <ted.vu@sourcecode.com.au>
2022-12-02 13:43:22 +0700

    AoC Day 2

--- 02/target/debug/deps/main-f8274a39a00ef029.d
+++
@@ -1,5 +0,0 @@
-/home/mhhmm/Documents/somewhereineurope/aoc/2022/02/target/debug/deps/main-f8274a39a00ef029.rmeta: src/main.rs
-
-/home/mhhmm/Documents/somewhereineurope/aoc/2022/02/target/debug/deps/main-f8274a39a00ef029.d: src/main.rs
-
-src/main.rs:
--- 02/target/debug/main.d
+++
@@ -1,1 +0,0 @@
-/home/mhhmm/Documents/somewhereineurope/aoc/2022/02/target/debug/main: /home/mhhmm/Documents/somewhereineurope/aoc/2022/02/src/main.rs
